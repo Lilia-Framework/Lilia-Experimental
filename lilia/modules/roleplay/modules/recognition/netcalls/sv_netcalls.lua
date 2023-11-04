d9bce623ecc98daaa7a332abc6c9191ef60a9a7d
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-04 23:56:41 +0100

    Base Lilia

---
+++ lilia/modules/roleplay/modules/recognition/netcalls/sv_netcalls.lua
@@ -0,0 +1,54 @@
+--------------------------------------------------------------------------------------------------------------------------
+netstream.Hook(
+    "rgnDirect",
+    function(client, target, name)
+        if target:GetPos():DistToSqr(client:GetPos()) > 100000 then return end
+        local CharID = client:getChar():getID()
+        if target:getChar():recognize(CharID, name) then
+            netstream.Start(client, "rgnDone")
+            hook.Run("OnCharRecognized", client, id)
+            client:notifyLocalized("recognized")
+        else
+            client:notifyLocalized("already_recognized")
+        end
+    end
+)
+
+--------------------------------------------------------------------------------------------------------------------------
+netstream.Hook(
+    "rgn",
+    function(client, level, name)
+        local targets = {}
+        if isnumber(level) then
+            local class = "w"
+            if level == 3 then
+                class = "ic"
+            elseif level == 4 then
+                class = "y"
+            end
+
+            class = lia.chat.classes[class]
+            for k, v in ipairs(player.GetAll()) do
+                if client ~= v and v:getChar() and class.onCanHear(client, v) then
+                    targets[#targets + 1] = v
+                end
+            end
+        end
+
+        if #targets > 0 then
+            local i = 0
+            local CharID = client:getChar():getID()
+            for _, v in ipairs(targets) do
+                if v:getChar():recognize(CharID, name) then
+                    i = i + 1
+                end
+            end
+
+            if i > 0 then
+                netstream.Start(client, "rgnDone")
+                hook.Run("OnCharRecognized", client, id)
+            end
+        end
+    end
+)
+--------------------------------------------------------------------------------------------------------------------------
