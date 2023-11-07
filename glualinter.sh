831f57aa6667c96d1a792a0632f8deeb7b8913ed
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-08 00:27:06 +0100

    .

---
+++ glualinter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Step 1: Checkout code for glualint
+echo "Step 1: Checking out code for glualint..."
+mkdir -p glualint
+cd glualint
+git clone https://github.com/YourUsername/YourRepository.git .
+cd ..
+
+# Step 2: Download glualint
+echo "Step 2: Downloading glualint..."
+curl -L -o glualint.zip https://github.com/FPtje/GLuaFixer/releases/download/1.26.0/glualint-1.26.0-x86_64-linux.zip
+unzip glualint.zip -d glualint
+
+# Step 3: Run glualint pretty-print
+echo "Step 3: Running glualint pretty-print..."
+cd glualint
+chmod +x glualint
+./glualint pretty-print
+
+echo "Script completed."
