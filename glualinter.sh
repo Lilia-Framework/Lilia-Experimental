fb47cda2a4ea470b985968924809d01875416fe1
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-08 08:39:38 +0000

    Updated script

--- glualinter.sh
+++ glualinter.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Step 1: Checkout code for glualint
-echo "Step 1: Checking out code for glualint..."
-mkdir -p glualint
-cd glualint
-git clone https://github.com/YourUsername/YourRepository.git .
-cd ..
-
-# Step 2: Download glualint
-echo "Step 2: Downloading glualint..."
+# Step 1: Download glualint
+echo "Step 1: Downloading glualint..."
 curl -L -o glualint.zip https://github.com/FPtje/GLuaFixer/releases/download/1.26.0/glualint-1.26.0-x86_64-linux.zip
 unzip glualint.zip -d glualint
 
-# Step 3: Run glualint pretty-print
-echo "Step 3: Running glualint pretty-print..."
-cd glualint
+# Step 2: Delete the zip file
+echo "Step 2: Deleting glualint.zip..."
+rm glualint.zip
+
+# Step 3: Move glualint executable to the root directory
+echo "Step 3: Moving glualint executable to the root directory..."
+mv glualint/glualint .
+
+# Step 4: Run glualint pretty-print
+echo "Step 4: Running glualint pretty-print..."
 chmod +x glualint
-./glualint --pretty-print	
-# Step 4: Delete the zip file
-echo "Step 4: Deleting glualint.zip..."
-rm ../glualint.zip
+./glualint --pretty-print
+
+# Step 5: Remove the glualint folder
+echo "Step 5: Removing the glualint folder..."
+rm -r glualint
 
 echo "Script completed."
