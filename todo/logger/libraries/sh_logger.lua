0ecf7a1ce08012fab01eb06042517fb0043b2685
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-12 07:58:08 -0800

    Moved Files & Started Logger

---
+++ todo/logger/libraries/sh_logger.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------------------------------------------------
+function MODULE:CharacterLoaded(id)
+    local character = lia.char.loaded[id]
+    local client = character:getPlayer()
+    lia.log.add(client, "charLoad", id, character:getName())
+end
+--------------------------------------------------------------------------------------------------------------------------
+function MODULE:OnCharacterDelete(client, id)
+    lia.log.add(client, "charDelete", id)
+end
+--------------------------------------------------------------------------------------------------------------------------
+function MODULE:OnCharCreated(client, character)
+    lia.log.add(client, "charCreate", character)
+end
+--------------------------------------------------------------------------------------------------------------------------
