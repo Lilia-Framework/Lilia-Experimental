2c1463d29375aa1a5de91142823ccd42bacb7889
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-23 04:40:45 -0800

    Moved Modules

---
+++ modules/permissions/sh_config.lua
@@ -0,0 +1,137 @@
+﻿------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.config.PhysGunMoveRestrictedEntityList = {"prop_dynamic","func_movelinear", "prop_door_rotating", "lia_vendor"}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.config.RemoverBlockedEntities = {"lia_bodygroupcloset", "lia_vendor",}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.config.DuplicatorBlackList = {"lia_storage", "lia_money"}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.config.RestrictedVehicles = {}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.config.CAMIPrivileges = {
+    {
+        Name = "Lilia - Staff Permissions - Admin Chat",
+        MinAccess = "admin",
+        Description = "Allows access to Admin Chat.",
+    },
+    {
+        Name = "Lilia - Staff Permissions - One Punch Man",
+        MinAccess = "superadmin",
+        Description = "Allows access to OPM to Ragdoll Minges.",
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn Ragdolls",
+        MinAccess = "admin",
+        Description = "Allows access to spawning ."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn SWEPs",
+        MinAccess = "superadmin",
+        Description = "Allows access to spawning SWEPs."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn Effects",
+        MinAccess = "admin",
+        Description = "Allows access to spawning Effects."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn Props",
+        MinAccess = "user",
+        Description = "Allows access to spawning Props."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn NPCs",
+        MinAccess = "superadmin",
+        Description = "Allows access to spawning NPCs."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Physgun Pickup",
+        MinAccess = "admin",
+        Description = "Allows access to picking up entities with Physgun."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Physgun Pickup on Restricted Entities",
+        MinAccess = "superadmin",
+        Description = "Allows access to picking up restricted entities with Physgun."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Physgun Pickup on Vehicles",
+        MinAccess = "admin",
+        Description = "Allows access to picking up Vehicles with Physgun."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Use Entity Properties on Blocked Entities",
+        MinAccess = "admin",
+        Description = "Allows access to using Entity Properties on Blocked Entities."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Can Remove Blocked Entities",
+        MinAccess = "admin",
+        Description = "Allows access to removing blocked entities."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - No Spawn Delay",
+        MinAccess = "admin",
+        Description = "Allows a user to not have spawn delay."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn Cars",
+        MinAccess = "admin",
+        Description = "Allows access to Spawning Cars."
+    },
+    {
+        Name = "Lilia - UserGroups - Staff Group",
+        MinAccess = "admin",
+        Description = "Defines Player as Staff."
+    },
+    {
+        Name = "Lilia - UserGroups - VIP Group",
+        MinAccess = "superadmin",
+        Description = "Defines Player as VIP."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Local Event Chat",
+        MinAccess = "admin",
+        Description = "Allows access to Local Event Chat."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Event Chat",
+        MinAccess = "admin",
+        Description = "Allows access to Event Chat."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn Restricted Cars",
+        MinAccess = "superadmin",
+        Description = "Allows access to Spawning Restricted Cars."
+    },
+    {
+        Name = "Lilia - Spawn Permissions - Can Spawn SENTs",
+        MinAccess = "admin",
+        Description = "Allows access to Spawning SENTs."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Can't be Grabbed with PhysGun",
+        MinAccess = "superadmin",
+        Description = "Allows access to not being Grabbed with PhysGun."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Can Grab World Props",
+        MinAccess = "superadmin",
+        Description = "Allows access to grabbing world props."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Can Grab Players",
+        MinAccess = "superadmin",
+        Description = "Allows access to grabbing players props."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Can Remove World Entities",
+        MinAccess = "superadmin",
+        Description = "Allows access to removing world props."
+    },
+    {
+        Name = "Lilia - Staff Permissions - Can Property World Entities",
+        MinAccess = "superadmin",
+        Description = "Allows access to propertying world props."
+    },
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
