42709baab75300446fc7ba43c7b593ffceb827bc
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-28 11:13:17 +0100

    Added some useful thirdparty libraries

--- lilia/libraries/meta/entity/cl_entitymeta.lua
+++ lilia/libraries/meta/entity/cl_entitymeta.lua
@@ -12,6 +12,7 @@ function entityMeta:getDoorPartner()
     for k, v in ipairs(ents.FindByClass("prop_door_rotating")) do
         if v:GetOwner() == self then
             self.liaDoorOwner = v
+
             return v
         end
     end
@@ -21,6 +22,7 @@ end
 function entityMeta:getNetVar(key, default)
     local index = self:EntIndex()
     if lia.net[index] and lia.net[index][key] ~= nil then return lia.net[index][key] end
+
     return default
 end
 
