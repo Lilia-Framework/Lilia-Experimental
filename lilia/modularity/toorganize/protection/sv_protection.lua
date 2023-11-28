e2a054948207ed8fe2b6819368baa235d701eb1d
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-28 11:21:48 +0100

    Added more production

--- lilia/modularity/toorganize/protection/sv_protection.lua
+++ lilia/modularity/toorganize/protection/sv_protection.lua
@@ -351,3 +351,42 @@ net.Receive(
     end
 )
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function MODULE:OnEntityCreated(eTarget)
+    if table.HasValue(lia.config.DevelopmentMaps, game.GetMap()) then return end
+    local sClass = eTarget:GetClass():lower():Trim()
+    if sClass == "lua_run" then
+        function eTarget:AcceptInput()
+            return true
+        end
+
+        function eTarget:RunCode()
+            return true
+        end
+
+        timer.Simple(
+            0,
+            function()
+                eTarget:Remove()
+            end
+        )
+    elseif sClass == "point_servercommand" then
+        timer.Simple(
+            0,
+            function()
+                eTarget:Remove()
+            end
+        )
+    end
+end
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function MODULE:PlayerInitialSpawn(client)
+    if game.SinglePlayer() then return end
+    timer.Simple(
+        0,
+        function()
+            if IsValid(client) == false or client:IsBot() or client:IsListenServerHost() or client.IsFullyAuthenticated == nil or client:IsFullyAuthenticated() then return end
+            client:Kick("Your SteamID wasn't fully authenticated, try restarting steam.")
+        end
+    )
+end
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
