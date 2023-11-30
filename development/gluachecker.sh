bb110ab712639a46f8993fcccde66543c173af9a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-30 16:40:40 +0000

    Updated a ton

---
+++ development/gluachecker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Setting up Lua..."
+sudo apt update && sudo apt install -y lua5.2 luarocks liblua5.2-dev
+
+echo "Cloning and building gluacheck..."
+git clone https://github.com/impulsh/gluacheck.git gluacheck &&
+cd gluacheck &&
+sudo luarocks make &&
+echo "Running gluacheck..."
+cd "/workspaces/Lilia-Experimental/lilia" && ls && sudo luacheck . --no-unused-secondaries --no-unused-args --no-redefined \
+  --no-unused --no-global --no-self -i 4.2/.*_ --formatter visual_studio \
+  --no-unused --no-max-line-length --no-max-code-line-length \
+  --no-max-string-line-length --no-max-comment-line-length \
+  --no-max-cyclomatic-complexity && cd ..
+
+echo "Cleaning up..."
+cd "/workspaces/Lilia-Experimental/development"
+sudo rm -rf gluacheck
+
+echo "Script completed successfully!"
