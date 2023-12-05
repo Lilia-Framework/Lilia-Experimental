1377dba7ff422e29377b639c85dc013eeba83d37
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-05 17:11:52 +0000

    Added some changes

--- lilia/modularity/core/protection/modules/antihacking/netcalls/server.lua
+++ lilia/modularity/core/protection/modules/antihacking/netcalls/server.lua
@@ -4,7 +4,7 @@ util.AddNetworkString("BanMeAmHack")
 net.Receive(
     "BanMeAmHack",
     function(_, client)
-        ProtectionCore:BackDoorApplyPunishment(client) 
+        ProtectionCore:BackDoorApplyPunishment(client)
     end
 )
 
@@ -17,7 +17,9 @@ for k, v in pairs(lia.config.KnownExploits) do
             if client.nextExploitNotify > CurTime() then return end
             client.nextExploitNotify = CurTime() + 2
             for _, p in pairs(player.GetAll()) do
-                if p:isStaff() then p:notify(client:Name() .. " (" .. client:SteamID() .. (v and ") may be attempting to crash the server!" or ") may be attempting to run exploits!")) end
+                if p:isStaff() then
+                    p:notify(client:Name() .. " (" .. client:SteamID() .. (v and ") may be attempting to crash the server!" or ") may be attempting to run exploits!"))
+                end
             end
         end
     )
