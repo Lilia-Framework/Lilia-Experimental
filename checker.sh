1befcc9d4f95aef336fa44420e57a77e0c8cf0d8
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-07 16:01:40 +0000

    Redefined Categories

---
+++ checker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Setting up Lua..."
+sudo apt update && sudo apt install -y lua5.2 luarocks liblua5.2-dev
+
+echo "Cloning and building gluacheck..."
+git clone https://github.com/impulsh/gluacheck.git gluacheck &&
+cd gluacheck &&
+sudo luarocks make &&
+echo "Running gluacheck..."
+cd "/workspaces/Lilia-Experimental/lilia" && sudo luacheck . --no-redefined \
+  --no-global --no-self \
+  --no-max-line-length --no-max-code-line-length \
+  --no-max-string-line-length --no-max-comment-line-length \ && cd ..
+
+echo "Cleaning up..."
+cd "/workspaces/Lilia-Experimental/development"
+sudo rm -rf gluacheck
+
+echo "Script completed successfully!"
--- lilia/modularity/essentials/revise/characters/recognition/pim/shared.lua
+++ lilia/modularity/essentials/revise/characters/recognition/pim/shared.lua
@@ -13,18 +13,16 @@ PIM:AddOption(
 )
 
 ----------------------------------------------------------------------------------------------
-if lia.config.FakeNamesEnabled then
-    PIM:AddOption(
-        "Allow This Player To Recognize You With A Fake Name",
-        {
-            runServer = false,
-            shouldShow = function(client, target)
-                local ourChar = client:getChar()
-                local tarCharID = target:getChar():getID()
-                return not hook.Run("IsCharRecognized", ourChar, tarCharID) and lia.config.FakeNamesEnabled
-            end,
-            onRun = function(_, target) if CLIENT then Derma_StringRequest("Allow this person to recognize you by a fake name.", "Enter a fake name to display to this player.", default or "", function(text) if text then netstream.Start("rgnDirect", target, text) end end) end end
-        }
-    )
-end
+PIM:AddOption(
+    "Allow This Player To Recognize You With A Fake Name",
+    {
+        runServer = false,
+        shouldShow = function(client, target)
+            local ourChar = client:getChar()
+            local tarCharID = target:getChar():getID()
+            return not hook.Run("IsCharRecognized", ourChar, tarCharID) and lia.config.FakeNamesEnabled
+        end,
+        onRun = function(_, target) if CLIENT then Derma_StringRequest("Allow this person to recognize you by a fake name.", "Enter a fake name to display to this player.", default or "", function(text) if text then netstream.Start("rgnDirect", target, text) end end) end end
+    }
+)
 ----------------------------------------------------------------------------------------------
