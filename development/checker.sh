ecc49872e57a24def8e099819ed622203e4643ef
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-10 01:58:34 +0000

    .

--- development/checker.sh
+++ development/checker.sh
@@ -8,11 +8,11 @@ git clone https://github.com/impulsh/gluacheck.git gluacheck &&
 cd gluacheck &&
 sudo luarocks make &&
 echo "Running gluacheck..."
-ls && cd .. && cd .. && ls && cd "lilia" && ls && luacheck . --no-redefined \
-  --no-global --no-self \
+cd "/workspaces/Lilia-Experimental/lilia" && sudo luacheck . --no-redefined \
+  --no-global --no-self -i 4.2/.*_ --formatter visual_studio \
   --no-max-line-length --no-max-code-line-length \
-  --no-max-string-line-length --no-max-comment-line-length \ && cd .. && ls
-
+  --no-max-string-line-length --no-max-comment-line-length \
+  --no-max-cyclomatic-complexity && cd ..
 
 echo "Cleaning up..."
 cd "/workspaces/Lilia-Experimental/development"
