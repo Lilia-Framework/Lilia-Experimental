84f7f8eaaa074381313f5969a1e1c853b7ead16c
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-10 02:06:12 +0000

    .

--- development/checker.sh
+++ development/checker.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+
+# Execute the workflow
+cd ..
+ls
+cd ..
+ls
 echo "Setting up Lua..."
 sudo apt update && sudo apt install -y lua5.2 luarocks liblua5.2-dev
 
 echo "Cloning and building gluacheck..."
-git clone https://github.com/impulsh/gluacheck.git gluacheck &&
-cd gluacheck &&
-sudo luarocks make &&
+git clone https://github.com/impulsh/gluacheck.git gluacheck
+cd gluacheck || exit
+sudo luarocks make
+ls
+cd ..
+ls
 echo "Running gluacheck..."
-cd "/workspaces/Lilia-Experimental/lilia" && sudo luacheck . --no-redefined \
-  --no-global --no-self -i 4.2/.*_ --formatter visual_studio \
+ls && cd "Lilia-Experimental/lilia" && luacheck . --no-redefined \
+  --no-global --no-self \
   --no-max-line-length --no-max-code-line-length \
-  --no-max-string-line-length --no-max-comment-line-length \
-  --no-max-cyclomatic-complexity && cd ..
+  --no-max-string-line-length --no-max-comment-line-length && cd .. && ls
 
 echo "Cleaning up..."
-cd "/workspaces/Lilia-Experimental/development"
+cd ..
 sudo rm -rf gluacheck
 
 echo "Script completed successfully!"
