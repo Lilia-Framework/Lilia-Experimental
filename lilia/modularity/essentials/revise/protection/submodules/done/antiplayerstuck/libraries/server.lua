6ef6a3ee44dee5fea84d47bafa9b6882f8bce7af
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-10 14:03:58 +0000

    Updated

--- lilia/modularity/essentials/revise/protection/submodules/done/antiplayerstuck/libraries/server.lua
+++ lilia/modularity/essentials/revise/protection/submodules/done/antiplayerstuck/libraries/server.lua
@@ -14,37 +14,41 @@ end
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function MODULE:CheckIfPlayerStuck()
-    for _, client in ipairs(player.GetAll()) do
-        if self:ShouldCheck(client) then
-            local Offset = Vector(5, 5, 5)
-            local Stuck = false
-            if client.Stuck then Offset = Vector(2, 2, 2) end
-            for _, ent in pairs(ents.FindInBox(client:GetPos() + client:OBBMins() + Offset, client:GetPos() + client:OBBMaxs() - Offset)) do
-                if self:ShouldCheck(ent) and ent ~= client and self:CanCollide(client, ent) then
-                    client:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
-                    local velocity = ent:GetForward() * 200
-                    client:SetVelocity(velocity)
-                    ent:SetVelocity(-velocity)
-                    Stuck = true
-                end
-            end
+    local function handleStuckPlayer(client)
+        local offset = client.Stuck and Vector(2, 2, 2) or Vector(5, 5, 5)
+        local stuck = false
 
-            if not Stuck then
-                client.Stuck = false
-                if client:GetCollisionGroup() ~= COLLISION_GROUP_PLAYER then
-                    client:SetCollisionGroup(COLLISION_GROUP_PLAYER)
-                    MsgC(Color(255, 155, 0), "[ANTI-STUCK] ", Color(255, 255, 255), "Changing collision group back to player for: " .. client:Nick())
-                end
+        for _, ent in pairs(ents.FindInBox(client:GetPos() + client:OBBMins() + offset, client:GetPos() + client:OBBMaxs() - offset)) do
+            if self:ShouldCheck(ent) and ent ~= client and self:CanCollide(client, ent) then
+                client:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+                local velocity = ent:GetForward() * 200
+                client:SetVelocity(velocity)
+                ent:SetVelocity(-velocity)
+                stuck = true
+            end
+        end
+
+        if not stuck then
+            client.Stuck = false
+            if client:GetCollisionGroup() ~= COLLISION_GROUP_PLAYER then
+                client:SetCollisionGroup(COLLISION_GROUP_PLAYER)
+                MsgC(Color(255, 155, 0), "[ANTI-STUCK] ", Color(255, 255, 255), "Changing collision group back to player for: " .. client:Nick() .. "\n")
             end
         end
     end
+
+    timer.Create("CheckIfPlayerStuck", 5, 0, function()
+        for _, client in ipairs(player.GetAll()) do
+            if self:ShouldCheck(client) then
+                handleStuckPlayer(client)
+            end
+        end
+    end)
 end
+
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function MODULE:ShouldCollide(ent1, ent2)
     if table.HasValue(self.BlockedCollideEntities, ent1:GetClass()) and table.HasValue(self.BlockedCollideEntities, ent2:GetClass()) then return false end
 end
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-timer.Create("CheckIfPlayerStuck", 5, 0, function() MODULE:CheckIfPlayerStuck() end)
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
