0e378da3c34937b4a7ca0bf81988c5a8ce225c56
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-19 15:43:24 +0000

    Added CanPlayerSwitchChar and Tweaked CanPlayerUseChar to be only on first load

--- lilia/modules/menus/submodules/mainmenu/netcalls/server.lua
+++ lilia/modules/menus/submodules/mainmenu/netcalls/server.lua
@@ -20,14 +20,15 @@ net.Receive(
 
         local id = net.ReadUInt(32)
         local character = lia.char.loaded[id]
+        local currentChar = client:getChar()
+
         if not character or character:getPlayer() ~= client then return response(false, "invalidChar") end
-        local status, result = hook.Run("CanPlayerUseChar", client, character)
+        local status, result = currentChar and hook.Run("CanPlayerSwitchChar", client, currentChar, character) or hook.Run("CanPlayerUseChar", client, character)
         if status == false then
             if result[1] == "@" then result = result:sub(2) end
             return response(result)
         end
 
-        local currentChar = client:getChar()
         if currentChar then currentChar:save() end
         hook.Run("PrePlayerLoadedChar", client, character, currentChar)
         character:setup()
