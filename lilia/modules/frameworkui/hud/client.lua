ce3a59758f504fa1a99e59a176cb33af0bed9d95
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-25 14:55:59 +0000

    Added isFaction CharMeta

--- lilia/modules/frameworkui/hud/client.lua
+++ lilia/modules/frameworkui/hud/client.lua
@@ -24,14 +24,3 @@ function FrameworkHUD:HUDPaint()
 end
 
 --------------------------------------------------------------------------------------------------------------------------
-concommand.Add(
-    "vgui_cleanup",
-    function()
-        for _, v in pairs(vgui.GetWorldPanel():GetChildren()) do
-            if not (v.Init and debug.getinfo(v.Init, "Sln").short_src:find("chatbox")) then v:Remove() end
-        end
-    end,
-    nil,
-    "Removes every panel that you have left over (like that errored DFrame filling up your screen)"
-)
---------------------------------------------------------------------------------------------------------------------------
